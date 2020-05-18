5e97d4cbffb2001927cbae7a106a273aa2ac1634
Alex Storer <astorer@GSB-C02Z64M7LVDQ.local>
2020-05-17 17:10:14 -0700

    support for python3 and updated URLs

--- get_reservoir_urllib2.py
+++ get_reservoir_urllib2.py
@@ -1,10 +1,20 @@
-import urllib2, csv, StringIO
+import sys
 
-elevationurl = "http://cdec.water.ca.gov/cgi-progs/queryCSV?station_id={sensor}&dur_code=D&sensor_num=6&start_date=1900/01/01&end_date=2100/01/01"
+if sys.version_info.major==3:
+    import urllib.request as urllib2
+    from io import StringIO
+elif sys.version_info.major==2:
+    import urllib2
+    from StringIO import StringIO
 
-capacityurl = "http://cdec.water.ca.gov/cgi-progs/queryCSV?station_id={sensor}&dur_code=D&sensor_num=15&start_date=1900/01/01&end_date=2100/01/01"
+import csv
 
-fsensors = open('reservoirs.csv','rU')
+# Taken from http://cdec.water.ca.gov/dynamicapp/wsSensorData
+elevationurl = "http://cdec.water.ca.gov/dynamicapp/req/CSVDataServlet?Stations={sensor}&SensorNums=6&dur_code=D&Start=1900/01/01&End=2100/01/01"
+capacityurl = "http://cdec.water.ca.gov/dynamicapp/req/CSVDataServlet?Stations={sensor}&SensorNums=15&dur_code=D&Start=1900/01/01&End=2100/01/01"
+
+
+fsensors = open('reservoirs.csv','r')
 dr = csv.DictReader(fsensors)
 
 
@@ -24,27 +34,28 @@ for d in dr:
 fcapacity.close()
 
 for code in reservoirs:
-    print code
+    print(code)
     if (code == code):        
         #r = requests.get(capacityurl.format(sensor=code))
         r = urllib2.urlopen(capacityurl.format(sensor=code))
+        content = r.read().decode('utf-8')
         #f = StringIO.StringIO(r.text)
-        f = StringIO.StringIO(r.read())
-        if len(f)<1000:
-            print "Failed to download ", code, " - not updating"
+        f = StringIO(content)
+
+        if len(content)<1000:
+            print("Failed to download ", code, " - not updating")
         else:
-            # skip the first two lines
-            next(f)
-            next(f)
-            dr = csv.DictReader(f,fieldnames=['Date','xxx','Volume'])
+            headers = ['STATION_ID','DURATION','SENSOR_NUMBER','SENSOR_TYPE','DATE TIME','OBS DATE']
+            headers += ['VALUE','DATA_FLAG','UNITS']
+            dr = csv.DictReader(f,fieldnames=headers)
             fw = open('{code}_pct.csv'.format(code=code),'w')
             dw = csv.DictWriter(fw,fieldnames=['Date','Percentage'])
             dw.writeheader()
             for d in dr:
                 row = dict()
-                row['Date'] = d['Date'][0:4]+'-'+d['Date'][4:6]+'-'+d['Date'][6:8]
+                row['Date'] = d['OBS DATE'][0:4]+'-'+d['OBS DATE'][4:6]+'-'+d['OBS DATE'][6:8]
                 try:
-                    row['Percentage'] = 100*float(d['Volume'])/reservoirs[code]['Capacity']
+                    row['Percentage'] = 100*float(d['VALUE'])/reservoirs[code]['Capacity']
                     if row['Percentage']>120 or row['Percentage']<1:
                         row['Percentage'] = 'm'
                 except:

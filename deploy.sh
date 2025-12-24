cd1ce791cc774b031a0dc35a7c51dbd4f2ec79f7
Alex Storer <alexstorer@gmail.com>
2025-12-24 14:39:33 -0800

    Add file listing generation and local server setup to deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,8 @@ source venv/bin/activate
 python3 get_reservoir_md.py
 python3 get_reservoir.py
 python3 build_page.py
+# find all the files in site/ and strip the leading directory names
+# make sure to remove filelist.txt if it exists
+rm -f site/filelist.txt
+find site/ -type f | sed 's|^site/||' > site/filelist.txt
 cd site && python3 -m http.server
